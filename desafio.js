23bb6d1c6bd9d85c7c256c3cbeaf63c8d651fa56
janissondev <janisson_matos10@hotmail.com>
2023-10-30 11:12:22 -0300

    Finalização do desafio com estrutura de decisões, operadores e ajustes

--- desafio.js
+++ desafio.js
@@ -1,7 +1,28 @@
-let vitorias = Number(prompt("Fala jogador! Informe a sua quantidade de vitórias"))
-let derrotas = Number(prompt("Agora, informe a quantidade de derrotas"))
+let victory = Number(prompt("Fala jogador! Informe a sua quantidade de vitórias"));
+let defeats = Number(prompt("Agora, informe a quantidade de derrotas"));
+let level;
 
-function calularPontuacao(vitorias, derrotas) {
-    let pontuacao = vitorias - derrotas
-    return pontuacao
+function calculateVictory(victory, defeats) {
+    let punctuation = victory - defeats;
+    return punctuation;
 }
+
+let victoryBalance = calculateVictory(victory, defeats);
+
+if (victoryBalance <= 10) {
+    level = "Ferro";
+} else if (victoryBalance >= 11 && victoryBalance <= 20) {
+    level = "Bronze";
+} else if (victoryBalance >= 21 && victoryBalance <= 50) {
+    level = "Prata";
+} else if (victoryBalance >= 51 && victoryBalance <= 80) {
+    level = "Ouro";
+} else if (victoryBalance >= 81 && victoryBalance <= 90) {
+    level = "Diamante";
+} else if (victoryBalance >= 91 && victoryBalance <= 100) {
+    level = "Lendário";
+} else if (victoryBalance >= 101) {
+    level = "Imortal";
+}
+
+console.log(`O Herói tem saldo de ${victoryBalance} vitórias e está no nível ${level}`)
